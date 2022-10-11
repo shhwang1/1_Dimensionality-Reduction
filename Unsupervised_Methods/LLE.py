f16d6d983d2f57f3a061c9d92fff49e054ce17a8
shhwang1 <115224653+shhwang1@users.noreply.github.com>
2022-10-11 15:35:07 +0900

    Update LLE.py

--- Unsupervised_Methods/LLE.py
+++ Unsupervised_Methods/LLE.py
@@ -1,16 +1,9 @@
-from re import I
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from matplotlib import offsetbox
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.decomposition import TruncatedSVD
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.manifold import LocallyLinearEmbedding
-from sklearn.neighbors import NeighborhoodComponentsAnalysis
-from sklearn.pipeline import make_pipeline
-from sklearn.random_projection import SparseRandomProjection
+from sklearn.preprocessing import MinMaxScaler
 
 def lle(args):
 
@@ -18,16 +11,20 @@ def lle(args):
 
     X_data = data.iloc[:, :-1]
     y_data = data.iloc[:, -1]
+    # scaler = MinMaxScaler()
+    # X_scaled = scaler.fit_transform(X_data)
 
-    lle = LocallyLinearEmbedding(n_components = args.n_components, n_neighbors = args.n_neighbors, random_state = args.seed)
-    lle.fit(X_data)
+    lle = LocallyLinearEmbedding(n_neighbors=args.n_neighbors,
+                            n_components = args.n_components,
+                            eigen_solver = 'auto',
+                            method='standard',)
+    mds_x = lle.fit_transform(X_data)
 
-    X_reduced = lle.transform(X_data)
-
-    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=y_data, cmap=plt.cm.hot)
-    plt.xlabel("$z_1$", fontsize=18)
-    plt.ylabel("$z_2$", fontsize=18)
-    plt.axis([-0.065, 0.055, -0.1, 0.12])
-    plt.grid(True)
-
-    plt.show()
+    plt.figure(figsize=(10,10))
+    for i in range(10):
+        plt.scatter(mds_x[y_data==i][:,0],
+                    mds_x[y_data==i][:,1],
+                    label = i)
+        plt.legend()
+    plt.title("LLE")    
+    plt.show()            
